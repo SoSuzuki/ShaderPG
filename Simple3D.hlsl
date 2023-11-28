5f27522a7d35d2051da702b73bd767fc49ab7e72
颯 鈴木 <220185@jc-21.jp>
2023-11-28 16:34:24 +0900

    ・緑になるけどなにこれ状態

--- Simple3D.hlsl
+++ Simple3D.hlsl
@@ -25,6 +25,7 @@ struct VS_OUT
 {
 	float4 pos		:	SV_POSITION;	//位置
 	float4 color	:	COLOR;			//色->輝度（明るさ）
+	float4 eyepos	:	POSITION;		//視点（カメラ位置）
 	float2 uv		:	TEXCOORD;		//UV座標
 };
 
@@ -32,7 +33,7 @@ struct VS_OUT
 //───────────────────────────────────────
 // 頂点シェーダ
 //───────────────────────────────────────
-VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
+VS_OUT VS(float4 pos : POSITION,float4 eyepos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 {
 	//ピクセルシェーダーへ渡す情報
 	VS_OUT outData;
@@ -41,19 +42,18 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 	//スクリーン座標に変換し、ピクセルシェーダーへ
 	outData.pos = mul(pos, matWVP);
 	outData.uv = uv;
-	float4 mulEyePos = eyePos - mul(pos, matNormal);
 	//法線を回転
 	normal = mul(normal, matNormal);
-
+#if 0
 	//float4 light = float4( 1.0, 0.8, -1.5, 0);	// 光源の向き（この座標から光源が"来る"）
 	float4 light = float4(-1, 0, 0, 0);
 	light = normalize(light);
-	//outData.color = clamp(dot(normal, light), 0, 1);// 範囲を絞る
-
-	
-	float4 reflect = 2 * normal * dot(normal, mulEyePos) - mulEyePos;
-	outData.color = clamp(reflect, 0, 1);
-
+	outData.color = clamp(dot(normal, light), 0, 1);// 範囲を絞る
+#else
+	outData.eyepos = eyePos - mul(pos,matNormal);
+	float4 reverseVec = 2 * normal * dot(normal, lightDirection) - lightDirection;
+	outData.color = saturate(reverseVec);
+#endif
 	//まとめて出力
 	return outData;
 }
@@ -63,37 +63,8 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 //───────────────────────────────────────
 float4 PS(VS_OUT inData) : SV_Target
 {
-	/*
 	float4 lightSource = float4(1.0,1.0,1.0,1.0);
 	float4 ambientSource = float4(0.2, 0.2, 0.2, 1.0);
-	float4 
-	float4 diffuse;
-	float4 ambient;
-	if (isTexture) {
-		// 拡散反射色（なんか明るいやつ）
-		diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * inData.color;
-		// 環境反射色（なんか暗いやつ）
-		ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambientSource;
-	}
-	else {
-		// 拡散反射色（なんか明るいやつ）
-		diffuse = lightSource * diffuseColor * inData.color;
-		// 環境反射色（なんか暗いやつ）
-		ambient = lightSource * diffuseColor * ambientSource;
-	}
-	return (diffuse + ambient);
-
-	*/
-	/*
-	// Postarization
-	float4 output = floor(g_texture.Sample(g_sampler,inData.uv) * 8.0) / 8;
-	return output;
-	*/
-
-	float4 lightSource = float4(1.0,1.0,1.0,1.0);
-	float4 ambientSource = float4(0.2, 0.2, 0.2, 1.0);
-	// 視線（カメラ位置）を正規化
-	float4 nEyePos = normalize(eyePos - matNormal);
 	float4 diffuse;
 	float4 ambient;
 	float4 specular;
@@ -103,13 +74,15 @@ float4 PS(VS_OUT inData) : SV_Target
 		// 環境反射色（なんか暗いやつ）
 		ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambientSource;
 		// 鏡面反射色（なんかツルツルなやつ）
-		specular = pow(saturate(dot(outData.color, nEyePos)), diffuseColor);
+		specular = pow(dot(inData.color, inData.eyepos - inData.pos), diffuseColor) * 2.0 * lightDirection;
 	}
 	else {
 		// 拡散反射色（なんか明るいやつ）
 		diffuse = lightSource * diffuseColor * inData.color;
 		// 環境反射色（なんか暗いやつ）
 		ambient = lightSource * diffuseColor * ambientSource;
+		// 鏡面反射色（なんかツルツルなやつ）
+		specular = pow(dot(inData.color, inData.eyepos - inData.pos), diffuseColor) * 2.0 * lightDirection;
 	}
 	return (diffuse + ambient + specular);
 }
